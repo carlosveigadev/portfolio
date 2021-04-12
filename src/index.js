67fbae9f9dbe4b2e29ccde1d8d0b945171ffacfb
wrack <carlosveiga1618@gmail.com>
2021-04-12 18:01:26 -0300

    add main files and create folder structure

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './Structure/Routes'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Routes />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
---
+++ src/structure/Routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/aboutme" component={About} />
+      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/contact" component={Contact} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
