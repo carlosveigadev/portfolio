e8bfb44eb38a98277876c0a3e5590615e5c53894
wrack <carlosveiga1618@gmail.com>
2021-04-14 18:39:50 -0300

    finish styling the home page

--- src/structure/Home.js
+++ src/structure/Home.js
@@ -1,8 +1,28 @@
+import { Center, Text, Circle } from '@chakra-ui/react';
+import {
+  FaGithubAlt, FaTwitter, FaLinkedinIn, FaAngellist,
+} from 'react-icons/fa';
 import Navbar from './Navbar';
+import style from '../assets/css/Home.module.css';
 
 const Home = () => (
   <>
-    <h1>Home Me</h1>
+    <Center className={style.homeText}>
+      <Text>Hello there, I am Carlos Veiga.</Text>
+      <Text>I am a Full Stack Web Developer.</Text>
+      <Text>
+        I can help you build a product, feature, or website. Look through some
+        of my work and experience!
+        If you like what you see and have a project you need coding,
+        don&apos;t hesitate to contact me.
+      </Text>
+      <Center className={style.icons}>
+        <Circle><FaGithubAlt /></Circle>
+        <Circle><FaTwitter /></Circle>
+        <Circle><FaLinkedinIn /></Circle>
+        <Circle><FaAngellist /></Circle>
+      </Center>
+    </Center>
     <Navbar />
   </>
 );
