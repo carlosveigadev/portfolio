3f7c66746c4921f8c42701d01939e51396486281
wrack <carlosveiga1618@gmail.com>
2021-04-15 19:40:56 -0300

    finish adding elements of the contact component and style it

--- src/structure/Contact.js
+++ src/structure/Contact.js
@@ -1,8 +1,24 @@
+import {
+  Button,
+  Center, FormControl, Input, Text, Textarea,
+} from '@chakra-ui/react';
 import Navbar from './Navbar';
+import style from '../assets/css/Contact.module.css';
 
 const Contact = () => (
   <>
-    <h1>Contact Me</h1>
+    <Center className={style.contact}>
+      <Text>
+        I am always interested in hearing about new projects,
+        so if you want to chat please get in touch.
+      </Text>
+      <FormControl>
+        <Input type="text" placeholder="Full Name" />
+        <Input type="email" placeholder="Email Address" />
+        <Textarea placeholder="Enter Text Here" />
+        <Button type="submit">Get In Touch</Button>
+      </FormControl>
+    </Center>
     <Navbar />
   </>
 );
