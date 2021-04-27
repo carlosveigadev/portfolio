d1b61fbf0319aee0fb90b9ba2a9e4328d4019702
wrack <carlosveiga1618@gmail.com>
2021-04-26 23:36:16 -0300

    add form config to project

--- src/structure/Contact.js
+++ src/structure/Contact.js
@@ -1,26 +1,55 @@
 import {
   Button,
-  Center, FormControl, Input, Text, Textarea,
+  Center, Input, Text, Textarea,
 } from '@chakra-ui/react';
+import { useState } from 'react';
 import Navbar from './Navbar';
 import style from '../assets/css/Contact.module.css';
 
-const Contact = () => (
-  <>
-    <Center className={style.contact}>
-      <Text>
-        I am always interested in hearing about new projects,
-        so if you want to chat please get in touch.
-      </Text>
-      <FormControl>
-        <Input type="text" placeholder="Full Name" />
-        <Input type="email" placeholder="Email Address" />
-        <Textarea placeholder="Enter Text Here" />
-        <Button type="submit">Get In Touch</Button>
-      </FormControl>
-    </Center>
-    <Navbar />
-  </>
-);
+const Contact = () => {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (event) => {
+    if (event.target.id === 'name') {
+      setName(event.target.value);
+    } else if (event.target.id === 'email') {
+      setEmail(event.target.value);
+    } else {
+      setMessage(event.target.value);
+    }
+  };
+
+  const onLoad = () => {
+    if (submitted) {
+      alert('Thanks for contacting me! I will get back as soon as possible.');
+    }
+  };
+
+  const onSubmit = () => {
+    setSubmitted(true);
+  };
+
+  return (
+    <>
+      <Center className={style.contact}>
+        <Text>
+          I am always interested in hearing about new projects,
+          so if you want to chat please get in touch.
+        </Text>
+        <iframe title="form" name="hidden_iframe" id="hidden_iframe" style={{ display: 'none' }} onLoad={onLoad} />
+        <form action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSc5FoMLAiDBSxSIDB38_yWngS5-K2zk7G5GNnMbmO60O5xocw/formResponse" method="post" target="hidden_iframe" onSubmit={onSubmit}>
+          <Input type="text" name="entry.2005620554" required placeholder="Full Name" id="name" value={name} onChange={handleChange} />
+          <Input type="email" name="entry.1045781291" required placeholder="Email Address" id="email" value={email} onChange={handleChange} />
+          <Textarea name="entry.839337160" required placeholder="Enter Text Here" value={message} onChange={handleChange} />
+          <Button type="submit">Get In Touch</Button>
+        </form>
+      </Center>
+      <Navbar />
+    </>
+  );
+};
 
 export default Contact;
