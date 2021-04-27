2eb62e4240b574519f38e1b3427c27831fb03c17
wrack <carlosveiga1618@gmail.com>
2021-04-27 15:43:59 -0300

    add some structure to about.js

--- src/structure/About.js
+++ src/structure/About.js
@@ -1,8 +1,25 @@
+import { Flex, Text, Button } from '@chakra-ui/react';
 import Navbar from './Navbar';
+import style from '../assets/css/About.module.css';
 
 const About = () => (
   <>
-    <h1>About Me</h1>
+    <Flex className={style.about}>
+      <Text>
+        Turning all my efforts into improving my
+        problem-solving skills and creativeness
+        on software and web development, and data analysis.
+      </Text>
+      <Button>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://docs.google.com/document/d/1gjztB6ql6_pvgPAUNiner61mTAu68g8QLrvKbTsCAbY/edit?usp=sharing"
+        >
+          Get My Resume
+        </a>
+      </Button>
+    </Flex>
     <Navbar />
   </>
 );
