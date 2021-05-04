b5a5a9e93b01634bafe0cbdb7e3e1d80858b23c8
wrack <carlosveiga1618@gmail.com>
2021-05-04 15:40:58 -0300

    fix change of backgrouond links and icons

--- src/structure/Projects.js
+++ src/structure/Projects.js
@@ -29,62 +29,67 @@ import style from '../assets/css/Projects.module.css';
 import Navbar from './Navbar';
 
 const Projects = () => {
-  // const [project, setProject] = useState('');
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [icons, setIcons] = useState([htmlLogo, cssLogo]);
   const [background, setBackground] = useState(`url(${gatheringsImg})`);
 
   const projects = [
     {
+      id: '0',
       title: 'Gatherings',
       liveLink: 'https://rawcdn.githack.com/wrakc/HTML-CSS-Capstone-Project/572a13c4445d57000b13ceae753b40a919cde4f8/index.html',
       githubLink: 'https://github.com/carlosveigadev/HTML-CSS-Capstone-Project.git',
       stacksNames: ['HTML', 'CSS'],
       logoLinks: [htmlLogo, cssLogo],
-      background: gatheringsImg,
+      background: `url(${gatheringsImg})`,
       text: 'In this HTML and CSS project, the main objective was to build reproduce the design of The Directory of Schools Webpage inspired by Mathew Njuguna and others on Behance, with a different theme. I chose to create a website named [gatherings] where people can meet in real life (after the COVID-19 situation) creating and sharing events across the region desired.',
     },
     {
+      id: '1',
       title: 'paliBOT',
       liveLink: 'https://polibot-web.herokuapp.com/',
       githubLink: 'https://github.com/carlosveigadev/palibot',
       stacksNames: ['Ruby', 'Ruby On Rails', 'TailwindCSS', 'Amazon S3', 'PostgreSQL'],
       logoLinks: ['https://www.vectorlogo.zone/logos/ruby-lang/ruby-lang-icon.svg', rubyOnRailsLogo, 'https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg', 'https://www.vectorlogo.zone/logos/amazon_aws/amazon_aws-icon.svg'],
-      background: palibotImg,
+      background: `url(${palibotImg})`,
       text: "This is a Ruby On Rails project developed at the end of the Microverse Main Technical Curriculum. It's a real-world-like project, built with business specifications to improve and test the achievement of technical and soft skills gained during this section of the program. The main objective was to reproduce the design of liFEsTlye. - Mobile version Webpage inspired by Nelson Sakwa on Behance on Behance, with a different theme. I chose to create a website named PoliBot (Filipino word for 'wanderer') where people can write articles about places they have visited on the continents of the earth.",
     },
     {
+      id: '2',
       title: '10 Minutes Office Workout',
       liveLink: '',
       githubLink: 'https://github.com/carlosveigadev/slack-10-min-office-workout',
       stacksNames: ['Ruby', 'Slack-Ruby-Bot'],
       logoLinks: ['https://www.vectorlogo.zone/logos/ruby-lang/ruby-lang-icon.svg', 'https://www.vectorlogo.zone/logos/slack/slack-icon.svg'],
-      background: officeWorkoutImg,
+      background: `url(${officeWorkoutImg})`,
       text: 'This bot was created as part of the Capstone project, which is a solo project at the end of each of the Microverse Main Technical Curriculum sections. This slack bot was built with Ruby, to randomly provide the user with a YouTube video to a 10 minutes workout in-home office.',
     },
     {
+      id: '3',
       title: "Clara's Quest Game",
       liveLink: 'https://carlosveigadev.github.io/claras-quest/',
       githubLink: 'https://github.com/carlosveigadev/claras-quest',
       stacksNames: ['JavaScript', 'Phaser 3'],
       logoLinks: [javascriptLogo, 'https://www.vectorlogo.zone/logos/phaserio/phaserio-icon.svg'],
-      background: clarasQuestImg,
+      background: `url(${clarasQuestImg})`,
       text: "This is a JavaScript and Phaser 3 library project developed at the end of the Microverse Main Technical Curriculum of the JavaScript section. It's a real-world-like project, built with business specifications to improve and test the achievement of technical and soft skills gained during this section of the program. The main objective was to design a game and implement it using the JavaScript Phaser 3 library.",
     },
     {
+      id: '4',
       title: 'Weather by GIFs',
       liveLink: 'https://carlosveigadev.github.io/weather-by-gifs/',
       githubLink: 'https://github.com/carlosveigadev/weather-by-gifs',
       stacksNames: ['JavaScript', 'Tailwind CSS'],
       logoLinks: [javascriptLogo, 'https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg'],
-      background: weatherImg,
+      background: `url(${weatherImg})`,
       text: 'In this project, I used JavaScript to learn and practice fetching data from APIs, then I rendered a simple webpage that shows the weather of a city provided by the input of the user.',
     },
   ];
 
   const handleClick = (e) => {
-    setIcons(projects[e.id].logoLinks);
-    setBackground(projects[e.id].background);
+    const index = e.target.id;
+    setIcons(projects[index].logoLinks);
+    setBackground(projects[index].background);
   };
 
   return (
@@ -103,13 +108,13 @@ const Projects = () => {
             <ButtonProject handleClick={handleClick} buttons={projects} />
           </Flex>
 
-          <Button
-            position="absolute"
+          <button
+            type="button"
             className={style.checkProjectButton}
             onClick={onOpen}
           >
             Check Project
-          </Button>
+          </button>
 
           <Modal isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
@@ -149,11 +154,10 @@ const ButtonProject = ({ buttons, handleClick }) => (
       <button
         className={style.button}
         type="button"
-        onClick={handleClick}
         key="buttons"
-        id={element.title}
+        onClick={handleClick}
       >
-        <span className={style.txtOverflow}>{element.title}</span>
+        <span id={element.id} className={style.txtOverflow}>{element.title}</span>
       </button>
     </>
   ))
