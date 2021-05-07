0c21b20847d5e4551ef40a66db1a5433cfa3b9a3
wrack <carlosveiga1618@gmail.com>
2021-05-07 20:19:55 -0300

    fix bg of body

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import Routes from './structure/Routes';
+import bgImg from './assets/images/background.jpg';
+
+const bg = document.getElementsByTagName('body')[0];
+bg.style.backgroundImage = `url(${bgImg})`;
+bg.style.backgroundRepeat = 'no-repeat';
+bg.style.backgroundSize = 'cover';
+bg.style.backgroundAttachment = 'fixed';
 
 ReactDOM.render(
   <React.StrictMode>
